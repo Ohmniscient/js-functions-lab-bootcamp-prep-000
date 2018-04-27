60929c6430376dfff0d8166fea1fa8485e0d8d7b
Cameron Orosco <cameron.orosco@outlook.com>
2018-04-27 02:23:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ function happyHolidays() {
 
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}`
+  var name = 'you'
 }
 
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
+  var holiday = 'Independence Day'
 }
 
 function holidayCountdown(holiday, days) {
   return `It\'s ${days} until ${holiday}`
+  var days = 'days'
+  var holiday = 'Mother\'s Day'
 }
